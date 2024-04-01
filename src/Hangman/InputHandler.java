7684b742377e83f041cb81e96a62f6ac93c9c09e
InterstellarDrift <InterstellarDrift@github.com>
2024-04-01 18:39:47 -0400

    Initial commit

---
+++ src/Hangman/InputHandler.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputHandler {
+
+    private static final BufferedReader INPUT_READER = new BufferedReader(
+            new InputStreamReader(System.in));
+
+
+    public static String promptInput(String prompt) {
+        System.out.println();
+        System.out.print(prompt);
+
+
+        String input;
+        try {
+            input = INPUT_READER.readLine();
+
+        } catch (IOException e) {
+            input = "-1";
+
+        }
+
+        return input;
+    }
+    public static int getPromptedID() {
+        int promptedID;
+        try {
+            promptedID = Integer.parseInt(promptInput("Please input an item ID: "));
+        } catch(NumberFormatException e) {
+            promptedID = -1;
+        }
+
+        return promptedID;
+    }
+}
